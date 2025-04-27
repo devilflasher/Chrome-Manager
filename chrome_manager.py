a7b13425adc8dc6f14afee560a5e2abf62a82258
Gatobate <chenycallen@gmail.com>
2025-04-28 05:24:11 +0800

    fix window_list

--- chrome_manager.py
+++ chrome_manager.py
@@ -93,7 +93,8 @@ class ChromeManager:
         # 存储进程ID和窗口编号的映射关系
         self.pid_to_number = {}
         self.window_list = None
-        
+
+    
         if not is_admin():
             if messagebox.askyesno("权限不足", "需要管理员权限才能运行同步功能。\n是否以管理员身份重新启动程序？"):
                 run_as_admin()
